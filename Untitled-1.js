449935747333b5090e49074debcd182d1f198a0e
Wilian Wagner <wilianwagnerwork@gmail.com>
2022-12-12 20:26:20 -0300

    codigo comentado

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,3 +1,6 @@
+//Código para gerar um numero aleatorio e fazer o usuario tentar acertar
+
+//variaveis e arrays
 var chute = document.querySelector("#chute")
 const b1 = document.querySelector("#b1")
 const resp = document.querySelector("h2")
@@ -6,6 +9,10 @@ let min = 0
 let max = 10
 let comparaçao = 0
 var repetidos = []
+
+
+
+//função que gera meu numero aleatorio
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -13,14 +20,18 @@ function getRandomIntInclusive(min, max) {
 }
 console.log(getRandomIntInclusive(min, max))
 var random = getRandomIntInclusive(min, max)
+
+
+//acionamento do botão chute do usuario
 b1.addEventListener("click", (e) => {
 
-
+//repetição para varias tentativas enquanto errado
     while (chute.value != random) {
-console.log(chute.value +"chute while")
+        console.log(chute.value + "chute while")
         console.log(chute.value)
         console.log(repetidos)
 
+        //teste para saber se há numeros repetidos
         var teste = repetidos.lastIndexOf(chute.value)
         console.log(teste + "teste")
         repetidos.push(chute.value)
@@ -32,19 +43,20 @@ console.log(chute.value +"chute while")
                 if (comparação == chute.value);
                 {
                     resp2.innerText = ("Esse numero já foi utilizado")
-                    
+
                 }
 
             })
         };
-
+//encerra o teste aqui
         resp.innerText = ("Tente novamente")
 
 
         break
     }
-    if(chute.value == random)
-    {
+    if (chute.value == random) {
         resp.innerText = ("Parabéns você acertou")
     }
 })
+
+//boa sorte corrigindo
